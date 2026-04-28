27c1a76c732185b3d10078135710d2d702379529
germa <german.kbrza@gmail.com>
2026-04-28 21:07:49 +0300

    5th commit

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from .models import Commentary
 
 
@@ -16,3 +17,13 @@ class CommentaryForm(forms.ModelForm):
                 "rows": 3
             }),
         }
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop("user", None)
+        super().__init__(*args, **kwargs)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if self.user is None or not self.user.is_authenticated:
+            raise ValidationError("You must be authenticated to post a comment.")
+        return cleaned_data
